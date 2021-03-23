c25b4f2334451aa5b347e30261cb0895adac5a21
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-23 20:24:17 +0000

    Character counter implemented successfully

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+let counter = 140;
+$("#tweet-text").on("keypress change", (event) => {
+  let charRemain = counter - $(event.target).val().length
+  $("output.counter").html(charRemain);
+  if (charRemain < 0) {
+    $("output.counter").addClass("negativeCount");
+  } if (charRemain > 0) {
+    $("output.counter").removeClass("negativeCount");
+  }
+})
+});
